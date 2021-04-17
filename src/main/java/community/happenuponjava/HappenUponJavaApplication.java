92c91d5921eea35c91a18fcb5c85cb8c727c185e
Louis Stevens <loustevenshere@gmail.com>
2021-04-17 11:43:53 -0400

    Day 0

---
+++ src/main/java/community/happenuponjava/HappenUponJavaApplication.java
@@ -0,0 +1,13 @@
+package community.happenuponjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappenUponJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappenUponJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/community/happenuponjava/HappenUponJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package community.happenuponjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappenUponJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
